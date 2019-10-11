ac49734793837a14ca7ad2bb323ad0c3fba5ee6a
Krijn van der Steen <krijnvdsteen@live.nl>
2019-10-11 18:39:59 +0200

    Fix inheritance

--- Card.cs
+++ Card.cs
@@ -3,86 +3,59 @@ using System.Windows.Forms;
 
 namespace Lab3
 {
-	public interface ICard
+	public abstract class ICard
 	{
-		void Connect();
-		void Disconnect();
-		int BeginTransaction(float amount);
-		bool EndTransaction(int id);
-		void CancelTransaction(int id);
-	}
+		public abstract void Connect();
+		public abstract void Disconnect();
+        public int BeginTransaction(float amount)
+        {
+            MessageBox.Show("Begin transaction 1 of " + amount + " EUR");
+            return 1;
+        }
+
+        public bool EndTransaction(int id)
+        {
+            if (id != 1)
+                return false;
+
+            MessageBox.Show("End transaction 1");
+            return true;
+        }
+
+        public void CancelTransaction(int id)
+        {
+            if (id != 1)
+                throw new Exception("Incorrect transaction id");
+
+            MessageBox.Show("Cancel transaction 1");
+        }
+    }
 
 	// Mock CreditCard implementation
 	public class CreditCard : ICard
 	{
-		public void Connect ()
+		public override void Connect ()
 		{
 			MessageBox.Show ("Connecting to credit card reader");
 		}
 
-		public void Disconnect ()
+		public override void Disconnect ()
 		{
 			MessageBox.Show ("Disconnecting from credit card reader");
-		}
-
-		public int BeginTransaction (float amount)
-		{
-			MessageBox.Show ("Begin transaction 1 of " + amount + " EUR");
-			return 1;
-		}
-
-		public bool EndTransaction (int id)
-		{
-			if (id != 1)
-				return false;
-
-			MessageBox.Show("End transaction 1");
-			return true;
-		}
-
-		public void CancelTransaction (int id)
-		{
-			if (id != 1)
-				throw new Exception ("Incorrect transaction id");
-
-			MessageBox.Show("Cancel transaction 1");
 		}
 	}
 
 	// Mock CreditCard implementation
 	public class DebitCard : ICard
 	{
-		public void Connect ()
+		public override void Connect ()
 		{
 			MessageBox.Show ("Connecting to debit card reader");
 		}
 
-		public void Disconnect ()
+		public override void Disconnect ()
 		{
 			MessageBox.Show ("Disconnecting from debit card reader");
-		}
-
-		public int BeginTransaction (float amount)
-		{
-			MessageBox.Show ("Begin transaction 1 of " + amount + " EUR");
-			return 1;
-		}
-
-		public bool EndTransaction (int id)
-		{
-			if (id != 1)
-				return false;
-
-			MessageBox.Show("End transaction 1");
-			return true;
-		}
-
-		public void CancelTransaction (int id)
-		{
-			if (id != 1)
-				throw new Exception ("Incorrect transaction id");
-
-			MessageBox.Show("Cancel transaction 1");
 		}
 	}
 }
